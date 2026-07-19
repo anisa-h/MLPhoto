296e642664c53107061da9b505ca7c102104af84
anisa-h <143896157+anisa-h@users.noreply.github.com>
2026-07-18 23:38:13 -0400

    i finished phase 3 walk the full folder, build the csv

--- photoinfo.py
+++ photoinfo.py
@@ -1,4 +1,6 @@
 import cv2
+import os 
+import csv
 from PIL import Image
 from PIL.ExifTags import TAGS 
 
@@ -7,12 +9,21 @@ def get_photo_info (path):
     image = Image.open(path) #holds the photo itself, path was only tthere to tell what image.open(path) was suppose to do.
     meta_data = image._getexif() 
     #loop time , it's the mechanism for reaching each individual pair, one at a time, so you can convert the tag ID into a readable name and print it clearly.
+    result = {}
+
     if meta_data is None:
         print("No EXIF data found.")
         return
     for tag_id, value in meta_data.items(): 
         tag_name = TAGS.get(tag_id, tag_id) #this converts a number into its word
-        return(tag_name, value)
+        if tag_name == "DateTimeOriginal":
+            result["date"] = value
+        if tag_name == "ExifImageWidth":
+            result["width"] = value
+        if tag_name == "ExifImageHeight":
+            result["height"] = value
+    
+    return result 
 get_photo_info("/Users/anisaraya/Desktop/repos/MLphoto/IMG_0179.JPG")
 
 def get_sharpness_score(path): 
@@ -22,6 +33,24 @@ def get_sharpness_score(path):
     return(laplacian.var())
 get_sharpness_score("/Users/anisaraya/Desktop/repos/MLphoto/IMG_0179.JPG")
 
-filename = "IMG_0179.jpg"
-if filename.lower().endswith((".jpg", ".jpeg", ".png", ".heic")):
-    # run some tests
+folder_path = "/Users/anisaraya/Desktop/repos/MLphoto"
+all_photo_data=[]
+for filename in os.listdir(folder_path):
+    if filename.lower().endswith((".jpg", ".jpeg", ".png", ".heic")):
+        full_path = os.path.join(folder_path, filename)
+
+        sharpness = get_sharpness_score(full_path)
+        info = get_photo_info(full_path)
+
+        row = {"filename": filename, "sharpness": sharpness}
+        row.update(info)
+
+        all_photo_data.append(row)
+
+with open("photos.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=all_photo_data[0].keys())
+    writer.writeheader()
+    writer.writerows(all_photo_data) 
+#open this file, call it f while I'm using it, and when I'm done with everything inside this indented block, close the file automatically, even if something goes wrong.
+
+print("done, wrote", len(all_photo_data), "rows")
